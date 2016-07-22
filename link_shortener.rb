19084482bca5d0142bd0bf0915ab68b5b2a64d0a
Brian Nickerson <nickerson.brian@gmail.com>
2016-07-22 18:52:28 -0400

    Finish id_look_up method and include commented out tests

--- link_shortener.rb
+++ link_shortener.rb
@@ -8,5 +8,27 @@ def link_shortener(link_id)
   short_url
 end
 
-id = 12345
-puts link_shortener(id)
+def id_look_up(short_url)
+  id = 0
+  short_url_ascii = short_url.bytes
+  short_url.length.times do |i|
+    if short_url[i] >= "a" && short_url[i] <= "z"
+      id = id * 62 + short_url_ascii[i] - 97
+    elsif short_url[i] >= "A" && short_url[i] <= "Z"
+      id = id * 62 + short_url_ascii[i] - 39
+    else short_url[i] >= "0" && short_url[i] <= "9"
+      id = id * 62 + short_url_ascii[i] + 4
+    end
+  end
+  id
+end
+
+# Uncomment to run tests
+
+# id = 12345
+# puts link_shortener(id) == "dnh"
+# puts id_look_up(link_shortener(id)) == 12345
+# puts "==============="
+# id = 332211
+# puts link_shortener(id) == "byAp"
+# puts id_look_up(link_shortener(id)) == 332211
