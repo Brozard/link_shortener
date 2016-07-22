96cb776301a14c77ce2d6c5a5839da49d85e6b36
Brian Nickerson <nickerson.brian@gmail.com>
2016-07-22 19:08:37 -0400

    Add comments

--- link_shortener.rb
+++ link_shortener.rb
@@ -1,16 +1,24 @@
 def link_shortener(link_id)
+  # Initialize string to use for Base 62 conversion
   convert_set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  # Initialize empty string to push converted IDs into
   short_url = ""
   until link_id == 0
+    # Convert the remainder value to Base 62 and add it to the beginning of 'short_url'...
     short_url.prepend(convert_set[link_id % 62])
+    # ...and then divide by 62, in turn dropping the remainder value.
     link_id /= 62
   end
+  # Return the converted ID
   short_url
 end
 
 def id_look_up(short_url)
+  # Initialize integer to store the ID
   id = 0
+  # Store the ASCII decimal values of each character in 'short_url'
   short_url_ascii = short_url.bytes
+  # Iterate through each character and increase the ID value accordingly
   short_url.length.times do |i|
     if short_url[i] >= "a" && short_url[i] <= "z"
       id = id * 62 + short_url_ascii[i] - 97
@@ -20,6 +28,7 @@ def id_look_up(short_url)
       id = id * 62 + short_url_ascii[i] + 4
     end
   end
+  # Return the ID for the original URL
   id
 end
 
