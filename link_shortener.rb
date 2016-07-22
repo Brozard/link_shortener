71e13ff953dcb7eed80894a8d661c80037e76120
Brian Nickerson <nickerson.brian@gmail.com>
2016-07-22 17:37:57 -0400

    Link shortener complete

--- link_shortener.rb
+++ link_shortener.rb
@@ -1,3 +1,12 @@
 def link_shortener(link_id)
   convert_set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  short_url = ""
+  until link_id == 0
+    short_url.prepend(convert_set[link_id % 62])
+    link_id /= 62
+  end
+  short_url
 end
+
+id = 12345
+puts link_shortener(id)
